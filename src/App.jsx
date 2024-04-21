8844c5cfd21fa5a94f0a93e259111b4fcaad23ee
Andrés <51636451+FairBreakPG@users.noreply.github.com>
2024-04-21 00:00:43 -0600

    Se añaden ajuste visuales

--- src/App.jsx
+++ src/App.jsx
@@ -17,9 +17,9 @@ function App() {
         <Header />
       </div>
       <div className="contenedor-principal">
-        <MyCard nombrePerro={'Yack'} src="https://cdn.pixabay.com/photo/2023/04/12/14/45/ai-generated-7920554_1280.jpg" descripcionCard={'Lleno de energía y listo para jugar. ¡Dale a Yack el hogar amoroso que se merece!'} tagText={'Salchicha'} tagColor={'success'} />
-        <MyCard nombrePerro={'Guardian'} src="https://cdn.pixabay.com/photo/2023/12/13/00/43/ai-generated-8446134_1280.jpg" descripcionCard={'Es juguetón, amigable y se lleva bien con mis niños y otros animales. ¡Haz de Guardian parte de tu familia hoy mismo '} tagText={'Pastor Aleman'} tagColor={"primary"}/>
-        <MyCard nombrePerro={'Max'} src="https://cdn.pixabay.com/photo/2024/03/08/18/01/ai-generated-8621330_1280.jpg" descripcionCard={'Un perro de tamaño grande con un corazón gigante. Hazte amigo de Max y experimenta un amor incondicional'} tagText={'raza rara'} tagColor={'danger'}/>
+        <MyCard nombrePerro={'Yack'} src="https://t2.ea.ltmcdn.com/es/razas/9/1/1/pastor-belga-groenendael_119_0_300_square.webp" descripcionCard={'Lleno de energía y listo para jugar. ¡Dale a Yack el hogar amoroso que se merece!'} tagText={'Pastor Velga'} tagColor={'success'} />
+        <MyCard nombrePerro={'Guardian'} src="https://t2.ea.ltmcdn.com/es/razas/5/1/7/goldendoodle_715_0_300_square.webp" descripcionCard={'Es juguetón, amigable y se lleva bien con mis niños y otros animales. ¡Haz de Guardian parte de tu familia hoy mismo '} tagText={'Goldendoodle'} tagColor={"primary"}/>
+        <MyCard nombrePerro={'Max'} src="https://t1.ea.ltmcdn.com/es/razas/6/3/0/bull-terrier-ingles_36_0_300_square.webp" descripcionCard={'Un perro de tamaño grande con un corazón gigante. Hazte amigo de Max y experimenta un amor incondicional'} tagText={'Buld Terrier'} tagColor={'danger'}/>
         <MyCard nombrePerro={'Tobi'} src="https://cdn.pixabay.com/photo/2023/04/02/17/26/animal-7895031_1280.jpg"descripcionCard={' Es un compañero leal y cariñoso que adora salir al bosque en la nieve ¡Ayuda a Tobi a encontrar una familia!'} tagText={'Siberiano'} tagColor={'warning'}/>
       </div>
       <Footer />
